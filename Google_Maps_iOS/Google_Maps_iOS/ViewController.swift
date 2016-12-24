d492bb1acd173ecfba52f9d39141a90297372095
Vatsal Rustagi <vatsalr23@gmail.com>
2016-12-24 11:20:36 +0530

    Added label (displays address), marker (appears in the center)

--- Google_Maps_iOS/Google_Maps_iOS/ViewController.swift
+++ Google_Maps_iOS/Google_Maps_iOS/ViewController.swift
@@ -11,17 +11,42 @@ import GoogleMaps
 
 class ViewController: UIViewController, GMSMapViewDelegate {
 
+    @IBOutlet weak var streetAddressLabel: UILabel!
     @IBOutlet weak var mapView: GMSMapView!
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
-        
         mapView.delegate = self
         let loc = CLLocationCoordinate2D(latitude: 33.6405, longitude: -117.8443)
         mapView.camera = GMSCameraPosition.camera(withTarget: loc, zoom: 15)
         
         super.viewDidLoad()
     }
-
+    
+    func getAddress(atCoordinate coordinate: CLLocationCoordinate2D)
+    {
+        let geocoder = GMSGeocoder()
+        
+        geocoder.reverseGeocodeCoordinate(coordinate){ response, error in
+            if let address = response?.firstResult(){
+                self.mapView.padding = UIEdgeInsets(top: self.topLayoutGuide.length, left: 0,bottom: 45, right: 0)
+                let lines = address.lines!
+                self.streetAddressLabel.text = lines[0]
+            }
+        }
+    }
+    
+    func mapView(_ mapView: GMSMapView, idleAt position: GMSCameraPosition) {
+        getAddress(atCoordinate: position.target)
+        
+        mapView.clear()
+        
+        let marker = GMSMarker(position: position.target)
+        marker.map = mapView
+        marker.title = "Current Location"
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
